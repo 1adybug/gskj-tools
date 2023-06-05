aa049ee522fc164d857a9a5a383f4e43cf018565
lurong <lurongv@qq.com>
2023-06-05 16:00:26 +0800

    更改 array.d.ts 位置

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,13 +6,3 @@ declare module "robust-segment-intersect" {
     export default function robustSegmentIntersect(a0: number[], a1: number[], b0: number[], b1: number[]): boolean
 }
 
-interface Array<T> {
-    with(this: Array<T>, index: number, value: T): Array<T>
-    toReversed(): Array<T>
-    toSorted(this: Array<T>, compareFn?: (a: T, b: T) => number): Array<T>
-    toSpliced(this: Array<T>, start: number, deleteCount?: number, ...items: T[]): Array<T>
-}
-
-declare interface Window {
-    Array: Array
-}
