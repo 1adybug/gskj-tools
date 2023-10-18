a3de18a40e2671b0c4d1da8176e3a1eaf0b79539
luzixu <lurongv@qq.com>
2023-10-18 14:41:35 +0800

    添加取余函数和drawArc方法

---
+++ dist/constant.d.ts
@@ -0,0 +1,315 @@
+/** Tailwind颜色 */
+export declare const tailwindColors: {
+    /** 石灰色 */
+    slate: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯灰色 */
+    gray: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 锌灰色 */
+    zinc: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 石板灰 */
+    neutral: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 砂岩灰 */
+    stone: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯红色 */
+    red: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 橘黄色 */
+    orange: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 琥珀色 */
+    amber: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯黄色 */
+    yellow: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 青橙色 */
+    lime: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯绿色 */
+    green: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 翡翠绿 */
+    emerald: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 盐湖绿 */
+    teal: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 亚蓝色 */
+    cyan: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 天蓝色 */
+    sky: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯蓝色 */
+    blue: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 靛青色 */
+    indigo: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 紫罗兰 */
+    violet: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 纯紫色 */
+    purple: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 丁香紫 */
+    fuchsia: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 粉红色 */
+    pink: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+    /** 玫瑰红 */
+    rose: {
+        "50": string;
+        "100": string;
+        "200": string;
+        "300": string;
+        "400": string;
+        "500": string;
+        "600": string;
+        "700": string;
+        "800": string;
+        "900": string;
+        "950": string;
+    };
+};
+export type TailwindColors = typeof tailwindColors;
+export type TailwindColorName = keyof TailwindColors;
+export type TailwindColorDepth = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
+export declare const tailwindColorNames: Record<TailwindColorName, string>;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import type { SetURLSearchParams } from "react-router-dom";
+export { tailwindColors, tailwindColorNames, TailwindColorDepth, TailwindColorName, TailwindColors } from "./constant";
 /**
  * 休眠指定时间
  * @param {number} time - 休眠的毫秒数
@@ -277,319 +278,29 @@ export declare const cssStore: Record<string, number>;
  * useCss
  */
 export declare function useCss(style: Record<string, string>): void;
-/** Tailwind颜色 */
-export declare const tailwindColors: {
-    /** 石灰色 */
-    slate: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯灰色 */
-    gray: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 锌灰色 */
-    zinc: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 石板灰 */
-    neutral: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 砂岩灰 */
-    stone: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯红色 */
-    red: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 橘黄色 */
-    orange: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 琥珀色 */
-    amber: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯黄色 */
-    yellow: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 青橙色 */
-    lime: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯绿色 */
-    green: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 翡翠绿 */
-    emerald: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 盐湖绿 */
-    teal: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 亚蓝色 */
-    cyan: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 天蓝色 */
-    sky: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯蓝色 */
-    blue: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 靛青色 */
-    indigo: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 紫罗兰 */
-    violet: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 纯紫色 */
-    purple: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 丁香紫 */
-    fuchsia: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 粉红色 */
-    pink: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-    /** 玫瑰红 */
-    rose: {
-        "50": string;
-        "100": string;
-        "200": string;
-        "300": string;
-        "400": string;
-        "500": string;
-        "600": string;
-        "700": string;
-        "800": string;
-        "900": string;
-        "950": string;
-    };
-};
-export type TailwindColors = typeof tailwindColors;
-export type TailwindColorName = keyof TailwindColors;
-export type TailwindColorDepth = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
-export declare const tailwindColorNames: Record<TailwindColorName, string>;
+/**
+ * 在 react 中比较数组是否发生变化
+ */
 export declare function useArraySignal<T>(data: T[], compareFn?: (a: T, b: T) => boolean): number;
+/**
+ * 取余函数
+ * @param a 被除数
+ * @param b 除数
+ */
+export declare function remain(a: number, b: number): number;
+export interface DrawArcOptions {
+    /** 到达绘制起点的方式，true 为连线，false 为移动，默认 false */
+    line?: boolean;
+    /** 起点到终点的方式，true 为逆时针，false 为顺时针，默认 false */
+    anticlockwise?: boolean;
+}
+/**
+ * 用 canvas 的方法画 path 的圆
+ * @param x 圆弧中心（圆心）的 x 轴坐标
+ * @param y 圆弧中心（圆心）的 y 轴坐标
+ * @param radius 圆弧的半径
+ * @param startAngle 圆弧的起始点，x 轴方向开始计算，单位以弧度表示
+ * @param endAngle 圆弧的终点，单位以弧度表示
+ * @param {DrawArcOptions} options 其他绘制选项
+ */
+export declare function drawArc(x: number, y: number, radius: number, startAngle: number, endAngle: number, options?: DrawArcOptions): string;
